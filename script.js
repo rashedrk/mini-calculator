2b3d9562d760665957cce8d792c4dfbc3b899cc5
rashedrk <rashedrk001@gmail.com>
2022-05-29 23:13:00 +0600

    operational

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function getHistory() {
-    return document.getElementById("cal-history").innerText;
+    return document.getElementById("history-value").innerText;
 }
 function printHistory(num) {
-    document.getElementById("cal-history").innerText=num;
+    document.getElementById("history-value").innerText=num;
 }
 function getOutput() {
-        return document.getElementById("cal-result").innerText;
+        return document.getElementById("output-value").innerText;
 }
 function getFormattedNum(num) {
+    if (num=="-") {
+        return "";
+    }
     var n=Number(num)
     var value = n.toLocaleString("en");
     return value;
@@ -16,11 +19,17 @@ function getNumber(num) {
     return Number(num.replace(/,/g,''));
 }
 function printOutput(num) {
-    document.getElementById("cal-result").innerText=getFormattedNum(num);
+    if (num=="") {
+        document.getElementById("output-value").innerText=num;
+    }
+    else{
+        document.getElementById("output-value").innerText=getFormattedNum(num);
+    }
+    
 }
 
 var number= document.getElementsByClassName("number")
-for (let i = 0; i < number.length; i++) {
+for (var i = 0; i < number.length; i++) {
     number[i].addEventListener("click",function() {
         var output=getNumber(getOutput());
         if (output != NaN) {
@@ -31,7 +40,7 @@ for (let i = 0; i < number.length; i++) {
     })   
 }
 var operator= document.getElementsByClassName("operator")
-for (let i = 0; i < operator.length; i++) {
+for (var i = 0; i < operator.length; i++) {
     operator[i].addEventListener("click",function() {
         if (this.id=="clear") {
             printHistory("");
@@ -40,11 +49,81 @@ for (let i = 0; i < operator.length; i++) {
         else if (this.id=="backspace") {
             var output=getNumber(getOutput()).toString();
             if (output!="") {
-                output=output.substr(0,output.length-1
-                    );
+                output=output.substr(0,output.length-1);
                 printOutput(output);
             }
         }
+        else {
+            var output=getOutput();
+            var history=getHistory();
+            
+             
+                switch (this.value) {
+                    case '+':
+                        printHistory(output + '+');
+                        printOutput("");
+                        break;
+                    case '-':
+                        if(output!="")
+                        {
+                        printHistory(output + '-');
+                        printOutput("");
+                        }
+                        else{
+                            printOutput("-");
+                        }
+                        break;
+                    case '/':
+                        printHistory(output + '/');
+                        printOutput("");
+                        break;
+                    case '*':
+                        printHistory(output + '*');
+                        printOutput("");
+                        break;
+                    case '%':
+                            printHistory(output + '%');
+                            printOutput("");
+                            break;
+                    case '=':
+                        var result=eval(history + output);
+                        printOutput(result);
+                        printHistory("");
+                            break;
+                    
+                }
+            
 
+
+
+
+
+
+
+
+
+
+/* 
+           else if (output=="" || history!="") {
+                if(isNaN(history[history.length-1])){
+                    history=history.substr(0,history.length-1)
+                }
+            }
+            if (output!="" || history!="") {
+                output= output==""?
+                output:getNumber(output);
+                
+                if (this.value="=") {
+                    var result=eval(history+output);
+                    printOutput(result);
+                    printHistory("");
+                }
+                else{
+                    history=history+this.value;
+                    printHistory(history);
+                    printOutput("");
+                }
+            }*/
+        } 
     })   
 }
